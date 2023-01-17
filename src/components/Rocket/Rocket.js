787c5633a8d9ea4cac107f6060295fb506f9bf86
Curtis <curtis.ch.chat@gmail.com>
2023-01-17 16:39:07 -0700

    building more routes for displaying tech info

--- src/components/Rocket/Rocket.js
+++ src/components/Rocket/Rocket.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react';
 // components
 import RockeHeader from './../RocketHeader';
 import TechInfo from './../TechInfo';
+// style
+import './style.scss';
 
 const Rocket = ({ rocket }) => {
     const { id, name, height, diameter, stages, cost_per_launch, engines } = rocket;
@@ -10,7 +12,9 @@ const Rocket = ({ rocket }) => {
     <Fragment>
         <div className="rocket d-flex flex-column align-items-center justify-content-around">
             <RockeHeader id={id} name={name} />
+            <TechInfo name={name} rocket={{ height, diameter, stages, cost: cost_per_launch }} isEngine />
         </div>
+        <TechInfo name={engine.type} engine={engines} isEngine />
     </Fragment>;
 };
 
